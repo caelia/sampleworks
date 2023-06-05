6a0be93b1de1053b5b8f60a04596832c121a86d0
Caelia R Chapin <crc@caelia.net>
2023-06-05 06:26:55 -0600

    Added ui::browser module.

---
+++ src/ui/browser.rs
@@ -0,0 +1,49 @@
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+/*
+use gtk4 as gtk;
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, FlowBox, Image};
+
+use std::fs::File;
+use std::io::BufReader;
+use rodio::{Decoder, OutputStream, source::Source};
+
+use std::collections::HashMap;
+
+pub struct DumbBrowser {
+    snd_dir: PathBuf,
+    img_dir: PathBuf,
+}
+
+impl DumbBrowser {
+    pub fn new(snd_dir: PathBuf, img_dir: PathBuf) -> DumbBrowser {
+        DumbBrowser { snd_dir, img_dir }
+    }
+
+    pub fn run(&self) -> glib::ExitCode {
+        let app = Application::builder()
+            .application_id("org.sampleworks.SWPrototype")
+            .build();
+
+        app.connect_activate(|app| {
+            let window = ApplicationWindow::builder()
+                .application(app)
+                .default_width(840)
+                .default_height(720)
+                .title("SampleWorks Prototype")
+                .build();
+
+            let fbox = gtk::Flowbox::builder()
+                .valign(gtk::Align::Start)
+                .max_children_per_line(9)
+                .min_children_per_line(2)
+                .selection_mode(gtk::SelectionMode::None)
+                .build();
+
+        })
+    }
+}
+*/
