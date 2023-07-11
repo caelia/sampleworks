64b0a32a90b65ae17d51d492cdee9a9c143d59fe
Caelia R Chapin <crc@caelia.net>
2023-07-11 03:21:45 -0600

    Added file hashing function using blake3.

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,12 @@
-use blake3::*;
+use blake3;
+use anyhow::{anyhow, Result};
 use std::path::PathBuf;
+use std::fs::read;
+use std::fmt::format;
 
-pub fn file_hash(path: &PathBuf) -> String {
-    todo!()
+pub fn file_hash(path: &PathBuf) -> Result<String> {
+    match read(path) {
+        Ok(bytes) => Ok(format!("{}", blake3::hash(&bytes))),
+        Err(e) => Err(anyhow!(e)),
+    }
 }
