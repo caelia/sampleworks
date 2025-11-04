eb6af1bef576ac36aaa69ee4b91417c5465eb718
Caelia R Chapin <crc@caelia.net>
2025-11-04 14:33:04 -0700

    Naive implementation of WaveThumb more or less done.

--- src/ui/wavethumb.rs
+++ src/ui/wavethumb.rs
@@ -2,45 +2,64 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
-use iced::advanced::{Widget, image::Image};
+// use iced::advanced::{image::Image};
+use iced::Element;
+use iced::widget::{text, image, mouse_area, column};
 
 use std::path::PathBuf;
 use std::rc::Rc;
 
-use crate::messaging::{ACReq, ACRsp, TxWrapper, RxWrapper};
+use crate::messaging::{ACReq, ACRsp, TxWrapper};
 
+#[derive(Debug, Clone)]
+pub enum Message {
+    Play(PathBuf),
+    Stop
+}
+
+#[derive(Debug, Clone)]
 pub struct WaveThumb {
-    file: PathBuf,
-    snd_dir: PathBuf,
-    img_dir: PathBuf,
-    req_tx: Rc<TxWrapper<ACReq>>,
-    rsp_rx: Rc<RxWrapper<ACRsp>>,
+    snd_file: PathBuf,
+    img_file: PathBuf,
+    // req_tx: Rc<TxWrapper<ACReq>>,
+    playing: bool,
 }
 
-impl Widget for WaveThumb {
-    fn size(&self) -> iced::Size {
-        
+impl WaveThumb {
+    fn new(
+        snd_file: PathBuf,
+        img_file: PathBuf,
+        // tx: TxWrapper<ACReq>,
+    ) -> Self {
+        // WaveThumb { snd_file, img_file, req_tx: Rc::new(tx), playing: false }
+        WaveThumb { snd_file, img_file, playing: false }
     }
 
-    fn layout(
-        &self,
-        tree: &mut iced::advanced::widget::Tree,
-        renderer: &iced::Renderer,
-        limits: &iced::advanced::layouts::Limits
-    ) -> iced::advanced::layout::Node {
-        
-    }
-
-    fn draw(
-        &self,
-        tree: &mut iced::advanced::widget::Tree,
-        renderer: &iced::Renderer,
-        theme: &iced::Theme,
-        style: &iced::advanced::renderer::Style,
-        layout: iced::advanced::Layout<'_>,
-        cursor: iced::mouse::Cursor,
-        viewport: &iced::Rectangle,
-    ) {
-        
+    fn toggle(&mut self) -> bool {
+        let new_state = !self.playing;
+        self.playing = new_state;
+        new_state
     }
 }
+
+pub fn view(thumb: &mut WaveThumb) -> Element<Message> {
+    let name = match thumb.snd_file.file_name() {
+        Some(nm) => nm.display().to_string(),
+        None => "Unknown".to_string(),
+    };
+    mouse_area(
+        column![
+            image(thumb.img_file.clone()),
+            text(name)
+        ]
+    )
+    .on_press(
+        if thumb.toggle() {
+            Message::Play(thumb.snd_file.clone())
+        } else {
+            Message::Stop
+        }
+    )
+    .into()
+}
+
